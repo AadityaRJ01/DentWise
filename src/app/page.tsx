b541eb46f8258d1e5c83170f7fd34c4efdeb4991
AadityaRJ01 <1ms23cs002@msrit.edu>
2026-03-04 22:45:32 +0530

    Agent and Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { SignOutButton, SignUpButton } from "@clerk/nextjs";
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+import PricingSection from "@/components/landing/PricingSection";
+import CTA from "@/components/landing/CTA";
+import { Footer } from "react-day-picker";
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <SignedOut>
-      <SignUpButton mode="modal">Sign Up</SignUpButton>
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton>Sign out</SignOutButton>
-      </SignedIn>
+    <div className="min-h-screen bg-background">
+      <Header/>
+      <Hero/>
+      <HowItWorks/>
+      <WhatToAsk/>
+      <PricingSection/>
+      <CTA/>
+      <Footer/>
     </div>
 
   );
